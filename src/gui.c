2fea1c8222dd79a2b69b9df913e775946f10b0dd
student <student@systems-vm.huawei.net>
2021-02-09 12:58:06 -0700

    changed fgets to getchar and putchar

--- src/gui.c
+++ src/gui.c
@@ -3,8 +3,18 @@
 int main()
 {
   char input[100];
-  if (fgets (input, sizeof(input), stdin)){
-    printf("%s",input);
+  int len = 0;
+
+  printf("Enter string: ");
+  input[len] = getchar();
+
+  while (input[len] != '\n'){
+   input[++len] = getchar();
+  }
+  input[++len] = '\0';
+
+  for (int i = 0; i < len; i++){
+    putchar(input[i]);
   }
   return 0;
 }
